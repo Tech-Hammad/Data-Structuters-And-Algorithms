8072891b7f038f0599fb563f503913709554e235
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-05-06 02:11:05 +0500

    Add files via upload

---
+++ Stack/Sort Stack/Stack.h
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+class Stack
+{
+protected:
+	T* arr;
+	int maxSize;
+	int currentSize;
+public:
+	Stack(int size);
+	virtual void Push(T value) = 0;
+	virtual T Pop() = 0;
+	virtual T Top() = 0;
+	virtual bool isEmpty() = 0;
+	virtual bool isFull() = 0;
+};
+
+template<typename T>
+Stack<T>::Stack(int size)
+{
+	arr = new T[size];
+	currentSize = 0;
+	maxSize = size;
+}
