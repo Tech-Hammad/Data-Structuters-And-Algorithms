f1086cc3e20b5120a0ec726804ddf2309dae2fca
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-06-02 10:31:09 +0500

    Add files via upload

---
+++ Reccursion/Reverse_String.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//void reverseString(string& str, int start, int end) 
+//{
+//    if (start >= end)
+//    {
+//        return; // Base case: when indices cross
+//    }
+//
+//    swap(str[start], str[end]);             // Swap characters
+//    reverseString(str, start + 1, end - 1); // Recursive call
+//}
+//
+//int main() 
+//{
+//    string str = "Hammad";
+//    cout << "Original String: " << str << "\n";
+//    reverseString(str, 0, str.length() - 1);
+//    cout << "Reversed string: " << str << endl;
+//
+//    return 0;
+//}
---
+++ Reccursion/Sum_Digits.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//int Sum_Digits(int num)
+//{
+//	if (num > 0)
+//	{
+//		return Sum_Digits(num / 10) + num % 10;
+//	}
+//	if (num == 0)
+//	{
+//		return 0;
+//	}
+//}
+//
+//int main()
+//{
+//	int number = 2347;
+//	cout << "The Count of Digits of " << number << " is: " << Sum_Digits(number) << "\n";
+//
+//	return 0;
+//}
