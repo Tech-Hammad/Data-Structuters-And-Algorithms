b6a1023b9cd016ddabf597fa232ce60043906cc7
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-06-02 10:37:17 +0500

    Add files via upload

--- Reccursion/Factorial.cpp
+++ Reccursion/Factorial.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int Factorial(int num)
+{
+	if (num > 0)
+	{
+		return num * Factorial(num - 1);
+	}
+
+	// Base case: factorial of 0 or 1 is 1
+	if (num == 0 || num == 1)
+	{
+		return 1;
+	}
+}
+
+int main()
+{
+	int number;
+	cout << "Enter the Number:- ";
+	cin >> number;
+	cout << "The Factorial of " << number << " is: " << Factorial(number) << "\n";
+
+	return 0;
+}
---
+++ Reccursion/Fibonacci_Series.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int num)
+{
+	if (num == 0)
+	{
+		return 0;
+	}
+	if (num == 1)
+	{
+		return 1;
+	}
+	return Fibonacci(num - 1) + Fibonacci(num - 2);
+}
+
+int main()
+{
+	int number;
+	cout << "Enter the Number:- ";
+	cin >> number;
+	cout << "The Fibonacci Series of " << number << " is: " << Fibonacci(number) << "\n";
+
+	cout << "The Fibonacci Series up to " << number << " is: ";
+	for (int i = 0; i <= number; i++) 
+	{
+		cout << Fibonacci(i) << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
