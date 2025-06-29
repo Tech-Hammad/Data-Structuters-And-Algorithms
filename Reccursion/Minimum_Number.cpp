474e9af2d3da2b1c563f0e9dd3d6138ad4b44269
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-06-30 02:00:41 +0500

    Update Minimum_Number.cpp

--- Reccursion/Minimum_Number.cpp
+++ Reccursion/Minimum_Number.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int Minimum(int arr[],int size)
+int findMinRec(int arr[], int n) 
 {
-	// Base Case
-	if (size == 1)
-	{
-		return arr[0];
-	}
-	return arr[size - 1] + Minimum(arr, size - 1);
+    if (n == 1)
+    {
+        return arr[0];
+    }
+
+    int restMin = findMinRec(arr, n - 1);
+	
+    if (arr[n - 1] < restMin) 
+    {
+        return arr[n - 1];
+    }
+    else
+    {
+        return restMin;
+    }
 }
 
-int main()
+int main() 
 {
-	const int size = 5;
-	int arr[size] = { 10,20,30,40,50 };
-	int result = Minimum(arr,size);
-	cout << "Result: " << result << "\n";
+    int arr[] = {5, 2, 8, 1, 9, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int minVal = findMinRec(arr, size);
+    cout << "Minimum value in array: " << minVal << endl;
 
-	return 0;
+    return 0;
 }
