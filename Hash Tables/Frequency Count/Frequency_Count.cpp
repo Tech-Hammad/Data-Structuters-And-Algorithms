d3d6dc6cfb791ed5f01f1bfc3151636f0d34511e
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-06-30 01:09:39 +0500

    Add files via upload

--- Hash Tables/Frequency Count/Frequency_Count.cpp
+++ Hash Tables/Frequency Count/Frequency_Count.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
 
+using namespace std;
+
+int main()
+{
+    unordered_map<string,int> umap;
+    string input;
+    ifstream fin;
+    fin.open("file.txt");
+
+    while (!fin.eof())
+    {
+        fin >> input;
+        umap[input]++;
+    }
+
+    for (auto x : umap)
+    {
+        cout << x.first << " " << x.second << endl;
+    }
+
+    return 0;
+}
